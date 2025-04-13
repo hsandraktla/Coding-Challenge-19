797fac20cc26abc0cec6815a37840aba4d0518a3
H Sandra Ktla <hsandraktla@usf.edu>
2025-04-13 17:05:24 -0400

    Setup root component with state and props structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//Acts as the main container
+import React, {useState} from "react";
+//Renders the Gallery component
+import Gallery from "./components/Gallery";
 
+//Root component of the app
 function App() {
-  const [count, setCount] = useState(0)
+  //Holds the state for all tours
+  const [tours, setTours] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  //Function to remove a book by its ID
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+  
+  return ( 
+    <main>
+      <h1>Our Tours</h1>
+      {/* Pass state and handlers to the Gallery component */}
+      <Gallery tours={tours} removeTour={removeTour} />
+    </main>
+  );
 }
 
-export default App
+//Exporting the App component
+export default App;
---
+++ src/components/TourCard.jsx
@@ -0,0 +1,6 @@
+import React, {useState} from "react";
+
+//TourCard renders individual tour details
+const TourCard = ({name, image, info, price, removeTour}) => {
+    //local state to toggle the 
+}
